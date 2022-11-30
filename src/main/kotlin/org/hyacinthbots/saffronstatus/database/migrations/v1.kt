46bff6f0fbb45044fa6a805e1bb7ede433190fb3
Caio <florianita100.mendes@gmail.com>
2022-11-30 18:28:20 -0300

    Migrations?

--- src/main/kotlin/org/hyacinthbots/saffronstatus/database/migrations/v1.kt
+++ src/main/kotlin/org/hyacinthbots/saffronstatus/database/migrations/v1.kt
@@ -6,9 +6,20 @@
 
 package org.hyacinthbots.saffronstatus.database.migrations
 
+import org.hyacinthbots.saffronstatus.database.entities.WatchedBotData
 import org.litote.kmongo.coroutine.CoroutineDatabase
+import org.litote.kmongo.exists
+import org.litote.kmongo.setValue
 
 @Suppress("UnusedPrivateMember")
 suspend fun v1(db: CoroutineDatabase) {
 	// TODO Fill with migration and set true in main function
+	with(db.getCollection<WatchedBotData>()) {
+		updateMany(
+			WatchedBotData::publishMessage exists false,
+			    setValue(
+			        WatchedBotData::publishMessage, false
+			)
+		)
+	}
 }
